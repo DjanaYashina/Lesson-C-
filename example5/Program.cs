fc79c5d84c5c5b656a902c1059e07c2060c61f0d
Djana Yashina <work1099@mail.ru>
2023-06-07 21:14:29 +0300

    добавлен файл example5

---
+++ example5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите Ваше имя ");
+string username = Console.ReadLine();
+if (username.ToLower() == "джана")
+{
+    Console.Write("Ура, это же Джана!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
