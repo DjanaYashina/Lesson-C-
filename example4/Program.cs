8956620cd806174a652b3edfe639bdeb56edfc6d
Djana Yashina <work1099@mail.ru>
2023-06-07 21:01:54 +0300

    добавлен файл example4

---
+++ example4/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1, 10); // рандомное число в диапазоне от 1 до 9
+Console.WriteLine(numberA);
+int namberB = new Random().Next(1, 10);
+Console.WriteLine(namberB);
+int result = numberA + namberB;
+Console.WriteLine(result);
